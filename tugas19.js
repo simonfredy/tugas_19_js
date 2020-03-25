55ca40146bbab0950bf4292dd760e2b07ed31d22
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-25 19:56:20 +0700

    Tugas 19 JS

---
+++ tugas19.js
@@ -0,0 +1,29 @@
+function tugasSembilanBelas() {
+    
+    // Tugas 1
+    var isi = "Semangat...";
+    console.log(isi.repeat(5));
+
+    //Tugas 2
+    var btg1 = "*****";
+    var btg2 = btg1.repeat(5);
+    var btg3 = btg1.repeat(10);
+    var btg4 = btg1.repeat(15);
+    var btg5 = btg1.repeat(20);
+    var btg6 = btg1.repeat(15);
+    var btg7 = btg1.repeat(10);
+    var btg8 = btg1.repeat(5);
+    var btg9 = "*****";
+
+    console.log(btg1);
+    console.log(btg2);
+    console.log(btg3);
+    console.log(btg4);
+    console.log(btg5);
+    console.log(btg6);
+    console.log(btg7);
+    console.log(btg8);
+    console.log(btg9);
+    
+}
+tugasSembilanBelas();
